c6f7dbef70f40a5fd0b98348c89a163bcec046fc
Yaw Karikari Wiafe <kaykayyaw@gmail.com>
2023-01-12 01:42:29 +0000

    fixed path error

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,8 +24,10 @@ module.exports = {
         josefin: ["Josefin Sans", "sans-serif"],
       },
       backgroundImage: {
-        "mobile-light-header-bg": "url('./public/bg-mobile-light.jpg')",
-        "mobile-dark-header-bg": "url('./public/bg-mobile-dark.jpg')",
+        "mobile-light-header-bg":
+          "url('../src/assets/images/bg-mobile-light.jpg')",
+        "mobile-dark-header-bg":
+          "url('../src/assets/images/bg-mobile-dark.jpg')",
       },
     },
   },
